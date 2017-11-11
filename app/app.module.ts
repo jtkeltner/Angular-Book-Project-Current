b2a2b0dc0ac597b17bf929bb2c0f4508fb8464ad
Justin <justin@nitroxmarketing.com>
2017-11-11 13:29:33 -0800

    cleaned up app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
-import { GitHubComponent } from './github.component';
-
-import { routing } from './app.routing';
-
 import { AboutMeComponent } from './aboutme.component';
 
-import { GitHubUserComponent } from './githubuser.component';
-import { LoginService } from './login.service';
+import { routing } from './app.routing';
 import { AuthGuard } from './auth-guard.service';
-import { LoginComponent } from './login.component';
 import { PreventUnsavedChangesGuard } from './prevent-unsaved-changes-guard.service';
+
+import { GitHubModule } from './github/github.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
@@ -32,15 +26,11 @@ import { PreventUnsavedChangesGuard } from './prevent-unsaved-changes-guard.serv
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    ReactiveFormsModule,
+    LoginModule,
+    GitHubModule,
     routing
   ],
   providers: [
-    AuthGuard,
-    LoginService,
-    PreventUnsavedChangesGuard
   ],
   bootstrap: [AppComponent]
 })

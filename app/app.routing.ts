0437ee94643d06f47454208b9c12e992aab12b65
Justin <justin@nitroxmarketing.com>
2017-11-11 13:34:36 -0800

    fixed routing.ts

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -3,14 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './notfound.component';
-import { GitHubComponent } from './github.component';
+import { GitHubComponent } from './github/github.component';
 import { AboutMeComponent } from './aboutme.component';
-import { GitHubUserComponent } from './githubuser.component';
+import { GitHubUserComponent } from './github/githubuser.component';
 
-import { LoginComponent } from './login.component';
-import { AuthGuard } from './auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './login/auth-guard.service';
 
-import { PreventUnsavedChangesGuard } from './prevent-unsaved-changes-guard.service';
+import { PreventUnsavedChangesGuard } from './login/prevent-unsaved-changes-guard.service';
 
 export const routing = RouterModule.forRoot([
 	{path: '', component: HomeComponent, canActivate: [AuthGuard]},

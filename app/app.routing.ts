7cd8230ee030cc146226829d3272aa951e3e9c36
Justin <justin@nitroxmarketing.com>
2017-11-11 01:16:49 -0800

    added login service

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -7,9 +7,11 @@ import { GitHubComponent } from './github.component';
 import { AboutMeComponent } from './aboutme.component';
 import { GitHubUserComponent } from './githubuser.component';
 
+import { AuthGuard } from './auth-guard.service';
+
 export const routing = RouterModule.forRoot([
 	{path: '', component: HomeComponent},
-	{path: 'GitHub', component: GitHubComponent},
+	{path: 'GitHub', component: GitHubComponent, canActivate: [AuthGuard]},
 	{path: 'GitHub/user/:login/:score', component: GitHubUserComponent},
 	{path: 'AboutMe', component: AboutMeComponent},
 	{path: '**', component: NotFoundComponent}
